c72316f827bfd3dbe7bed5dbdba8bb86cab6f4e5
Likith Naga Sai Adusumalli <likithadusumalli@gmail.com>
2026-01-30 22:11:20 +0530

    Added proxy.ts(middleware.ts) for routing

---
+++ src/proxy.ts
@@ -0,0 +1,26 @@
+import { getSession } from "@/lib/auth/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(req: NextRequest) {
+  // middleware.ts was named as proxy.ts from Next.js 16 onwards
+  const { pathname } = req.nextUrl;
+
+  const authPages = ["/", "/sign-in", "/sign-up"];
+  const protectedPages = ["/dashboard"];
+
+  const session = await getSession();
+
+  if (session && authPages.includes(pathname)) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!session && protectedPages.some((p) => pathname.startsWith(p))) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/sign-in", "/sign-up", "/dashboard"],
+};
