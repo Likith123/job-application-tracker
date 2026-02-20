b57e959887ac7f189d8ae2f86cdc098a4565a188
Likith Naga Sai Adusumalli <likithadusumalli@gmail.com>
2026-02-20 18:48:29 +0530

    Updated layout.tsx for updated SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,11 +58,11 @@ export const metadata: Metadata = {
     title: "JoAT : Your Job Applications Tracker",
     description:
       "Track job applications in real-time. Stay organized throughout your job search.",
-    url: "https://jobat.vercel.app",
+    url: "https://yourjoat.vercel.app",
     siteName: "JoAT",
     images: [
       {
-        url: "https://jobat.vercel.app/og-image.png",
+        url: "https://yourjoat.vercel.app/og-image.png",
         width: 1200,
         height: 630,
         alt: "JoAT : Job Applications Tracker",
@@ -76,7 +76,7 @@ export const metadata: Metadata = {
     title: "JoAT : Your Job Applications Tracker",
     description:
       "Real-time job application tracking. Stay organized and land interviews.",
-    images: ["https://jobat.vercel.app/og-image.png"],
+    images: ["https://yourjoat.vercel.app/og-image.png"],
     creator: "@LikithDeveloper",
     site: "@LikithDeveloper",
   },
